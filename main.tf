1782c8008c40de5b716b03ce8a9abbbd1d8095ff
prabala <radhakrishnaprabala8@gmail.com>
2024-10-02 23:13:59 +0530

    main.tf vr2

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
-resource "aws_instance" "inst" {
-  ami           = data.aws_ami.ami.id
+#resource "aws_instance" "inst" {
+#  ami           = data.aws_ami.ami.id
+#  instance_type = var.instance_type
+#  subnet_id     = var.subnet_id
+#}
+
+resource "aws_launch_template" "template" {
+  name_prefix   = "${var.name}-${var.env}-alt"
+  image_id      = data.aws_ami.ami.id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name               = "${var.name}-${var.env}-asg"
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
 }
