5b20449a61d3e51bfe688b56a074742c9aeb6add
prabala <radhakrishnaprabala8@gmail.com>
2024-10-06 07:45:48 +0530

    vars.tf vr2

--- vars.tf
+++ vars.tf
@@ -1,11 +1,16 @@
 variable "instance_type" {}
+
 variable "subnet_id" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+
 variable "env" {}
+variable "bastion_cidr" {}
+
 variable "name" {}
+
 variable "desired_capacity" {}
 variable "max_size" {}
 variable "min_size" {}
-variable "vpc_id" {}
-variable "allow_app_cidr" {}
-variable "bastion_cidr" {}
+
 variable "tags" {}
