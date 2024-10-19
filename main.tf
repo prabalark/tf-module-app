7c2ad17da205340a7815bc8a96e69acd3e64cdfe
prabala <radhakrishnaprabala8@gmail.com>
2024-10-19 16:48:11 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_autoscaling_group" "asg" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${var.name}-${var.env}-TG"
+  name     = "${var.name}-${var.env}-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
