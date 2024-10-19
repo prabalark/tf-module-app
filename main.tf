e9aebdbbad4a127597b5f6fb6def1d78b57cd8c2
prabala <radhakrishnaprabala8@gmail.com>
2024-10-19 16:53:39 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -44,11 +44,12 @@ resource "aws_launch_template" "template" {
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name               = "${var.name}-${var.env}-asg"
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  name                = "${var.name}-${var.env}-asg"
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
