72eef825535b5dcc1b67a3c77b670e5db5d5b031
prabala <radhakrishnaprabala8@gmail.com>
2024-10-20 17:53:56 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_lb_listener_rule" "rule" {
 resource "aws_route53_record" "main" {
   zone_id = var.domain_id   # Route53 : Hosted zone ID
   name    = var.dns_name  # Route53 : record name
-  records = [var.lb_dns_name] # frnt,cata...
+  records = [var.lb_dns_name] # DNS-name in Load balancers [ public & private ]
   type    = "CNAME"  # before we take A rec
   ttl     = 30
 }
