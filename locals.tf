08efdb9ca208d7c0e15b7904349e8709614de27d
prabala <radhakrishnaprabala8@gmail.com>
2024-10-20 17:30:59 +0530

    rootmodule.tf

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   asg_tags = merge(var.tags, { Name="${var.name}-${var.env}"} )
   dynamic_asg_tags = [for k ,v in local.asg_tags : tomap({ key =k , value =v })]
 
-  dns_name = "${var.name}-${var.env}.${var.domain_name}"
+  dns_name = "${var.dns_name}.${var.domain_name}"
   # in router53 : only for frontend we dnt require starting frnt.dev -> dev.de72..
   # remaining cata.dev.de72 etc for this in rootmodule kept condition
   # otherwise give variable in main.tfvars give names
