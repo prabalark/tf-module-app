0470c39ed8d3990b02ac72b85d22328a5469007c
prabala <radhakrishnaprabala8@gmail.com>
2024-10-20 11:06:10 +0530

    main.tfvars

--- main.tf
+++ main.tf
@@ -74,3 +74,21 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
   tags     = merge(var.tags, { Name = "${var.name}-alb-${var.env}" })
 }
+
+  # To listener adding rules ,
+  # if any touch listener then forward to TG
+resource "aws_lb_listener_rule" "rule" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [local.dns_name]
+    }
+  }
+}
