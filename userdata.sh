14879c16753b4815c49808dc5976ddf1d7e7cbe2
prabala <radhakrishnaprabala8@gmail.com>
2024-10-21 22:00:35 +0530

    userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# informing this as shell-script
+
+# directly connecting shell - rabbitmq and connect by above code
+labauto ansible
+
+ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name = ${name} -e env =${env} &>>/opt/ansible.log
