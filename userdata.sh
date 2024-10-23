32b32163ed18cca4ac5c46a52f6ac84c43cfa985
prabala <radhakrishnaprabala8@gmail.com>
2024-10-23 20:32:19 +0530

    userdata.sh vr1

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,5 @@
 
 # directly connecting shell - rabbitmq and connect by above code
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name = ${name}  &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/prabalark/roboshop-ansible-72.git roboshop-ani.yml -e role_name = ${name} -e env =${env} &>>/opt/ansible.log
 
