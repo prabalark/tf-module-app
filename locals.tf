5abae559cfb0b98a079572f5074f46feaffa3ba2
prabala <radhakrishnaprabala8@gmail.com>
2024-10-29 11:27:21 +0530

    local.tf

--- locals.tf
+++ locals.tf
@@ -7,6 +7,6 @@ locals {
   # remaining cata.dev.de72 etc for this in rootmodule kept condition
   # otherwise give variable in main.tfvars give names
 
-   parameters = concat(var.parameters, [var.name])
+   parameters = concat(var.parameters,[var.name])
     resources  = [for parameter in local.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${parameter}.*"]
 }
